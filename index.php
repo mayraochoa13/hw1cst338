f9e7a926ec1a210501ac65fc28d1fb8d5f090626
Mayra Ochoa <mayochoa@csumb.edu>
2018-02-08 06:09:24 +0000

    Homework1

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <title>Mayra Ochoa: Machine Learning</title>
+        <link href="https://fonts.googleapis.com/css?family=Merriweather" rel="stylesheet">
+        
+        <link href = "css/style.css" rel ="stylesheet" type ="text/css" />
+    </head>
+    <body>
+        <header>
+                    <strong>Machine Learning</strong>
+                    <figure id ="brain">
+                        <img width = "100" height = "100" alt="affective" src = "images/brain.png"/>
+                    </figure>
+        </header>
+        <nav class="pag">
+            <hr/>
+            <a href = "index.php" style = "color:white;">Overview  </a>
+            <a href = "theory.html">Theory</a>
+            <a href = "advances.html">Advances</a>
+            <a href = "applications.html">Applications</a>
+        </nav>
+        
+        <main>
+              <div>
+                <figure id="machine">
+                  <img width = "350" height = "350" alt="affective" src = "images/machine.jpg"/>
+                </figure>
+                <pre id="Text1">
+                    <b>Overview</b>
+                    Throught the time, computer science field has grown. Machine learning as one of the methods that gives the ability 
+                    to models to learn and adapt trhough experince. In 1959, an American pioneer in artificial intelligence, brought 
+                    out the term of "Machine Learning". His name was  Arthur Samuel. Machine learning explores the study and construction
+                    of algorithms that make prediction on data and data-driven decisions. Which mean that builds a model from different
+                    sample inputs and predictates the functionality and effective predictions. It is employed in a range of computing
+                    task where programming some explicit algorithms with a better perfomance including different detection of networks,
+                    application and infiltrations towards a data breach opticalcharacter recognition (OCR), learning to rank, and 
+                    computer vision.
+                    
+                </pre>
+            </div>
+                 <a href="http://usblogs.pwc.com/emerging-technology/a-look-at-machine-learning-infographic/" target="_blank">Source 1</a> 
+                 <a href="https://en.wikipedia.org/wiki/Machine_learning" target="_blank">Source 2</a> 
+        </main>
+        <footer>
+            <hr>
+            <figure id="csumb">
+                    <img width = "200" height = "150" alt="affective" src = "https://experiencecle.com/wp-content/uploads/2016/03/csumb_logo-1.png"/><br/>
+                     CST 336 Internet Programming. 2018© <br/>
+                     <strong>Disclaimer:</strong> the information in this webpage is fictitous. <br/>
+                     It is used for academic purposes only.
+            </figure>
+        </footer>
+    </body>
+</html>
